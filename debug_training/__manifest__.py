1b12dd15057d629528e6cc4d58d124a5a2a841bc
riaa-odoo <riaa@odoo.com>
2024-01-30 16:49:23 -0600

    [ADD] debug_training

---
+++ debug_training/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Debug Training",
+    "summary": "Debug Odoo Framework (Py, XML and JS) using VSCode and Chrome",
+    "description": """
+        Debug Odoo Framework (Py, XML and JS) using VSCode and Chrome.
+    """,
+    "version": "1.0.0",
+    "category": "Custom Developments",
+    "license": "OPL-1",
+    "author": "RIAA",
+    "website": "www.odoo.com",
+}
